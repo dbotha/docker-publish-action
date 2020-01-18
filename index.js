2384f4f00440f20d7d1bff796d01a649868fc1fd
Deon Botha <deon@dbotha.com>
2020-01-18 22:39:29 +0000

    ls

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const exec = require("@actions/exec");
+const io = require("@actions/io");
 
 async function main() {
     try {
-        const output = await exec.exec("docker --version");
-        console.log(`Output: ${output}`);
+        const out = await exec.exec("ls");
+        // const output = await exec.exec("docker --version");
+        // console.log(`Output: ${output}`);
     } catch (error) {
         core.setFailed(error.message);
     }

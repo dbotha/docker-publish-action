5c68a89a95956c1507333708541178d1330074d1
Deon Botha <deon@dbotha.com>
2020-01-18 22:47:57 +0000

    Attempting to run docker image build

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ const io = require("@actions/io");
 
 async function main() {
     try {
-        const out = await exec.exec("ls");
-        // const output = await exec.exec("docker --version");
+        const dockerRegistry = core.getInput("docker-registry");
+        const dockerImageName = core.getInput("docker-image-name");
+        const dockerImageTagsString = core.getInput("docker-image-tags");
+        const username = core.getInput("username");
+        const password = core.getInput("password");
+        const tags = dockerImageTagsString.split(",").map(tag => tag.trim());
+        if (tags.length === 0) tags.push("latest");
+        const out = await exec.exec(`docker image build -t ${dockerImageName}:${tags[0]} .`);
+        const output = await exec.exec("docker image ls");
         // console.log(`Output: ${output}`);
     } catch (error) {
         core.setFailed(error.message);
